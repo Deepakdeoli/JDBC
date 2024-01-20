6aab1c676a6e0000c5983d1e8a7821283def7d22
Deepak_Gurjar <deepakdeoli00@gmail.com>
2024-01-20 23:05:38 +0530

    20-01.23

---
+++ update.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+public class update {
+    public static void main(String[] args) {
+     try{
+        Connection con =ConnectionProvider.getConnection();
+        
+    }   
+    }
+}
